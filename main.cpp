16be4df5090c9c74f55a5a262df0c26cffb94022
ALFTavares <alftavares1985@gmail.com>
2016-12-06 17:39:45 +0000

    Update main.cpp
    changed the sort function for 2 for loops and was able to modify the 2 arrays in the same cicle

--- main.cpp
+++ main.cpp
@@ -20,31 +20,37 @@
  Person 5: ate 0 pancakes*/
 
 #include <iostream>
-#include <algorithm>
-#include <functional>
-
-struct pancakes
-{
-    int person[10]={1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int pancake[10];
-};
 
 int main()
 {
-    pancakes pan;
-    
-    int i;
-    
-    for(i=0; i<10; i++)
-    {
-        std::cout << "Number of pancakes person " << pan.person[i] << " ate: ";
-        std::cin >> pan.pancake[i];
-    }
-    
-    std::sort(pan.pancake, pan.pancake+10, std::greater<int>());
-    
-    for(i=0; i<10; i++)
-        std::cout << "person " << pan.person[i] << ": " << pan.pancake[i] << " pancakes" << std::endl;
-    
-    return 0;
+	int person[10]={1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	int pancake[10];
+	int i, j, temp;
+
+	for(i=0; i<10; i++)
+	{
+		std::cout << "Number of pancakes person " <<person[i] << " ate: ";
+		std::cin >> pancake[i];
+	}
+
+	for(i=0; i<10; i++)
+	{
+		for(j=i; j<10; j++)
+		{
+			if(pancake[i]<pancake[j])
+			{
+				temp=pancake[i];
+				pancake[i]=pancake[j];
+				pancake[j]=temp;
+				temp=person[i];
+				person[i]=person[j];
+				person[j]=temp;
+			}
+		}
+	}
+
+	for(i=0; i<10; i++)
+	{
+		std::cout << "person " << person[i] << ": " << pancake[i] << " pancakes" <<std::endl;
+	}
 }
